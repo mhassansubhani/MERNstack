429f561a4d03c29524b2107689b1e24b88d6dfe7
mhassansubhani <hassansubhani8@hotmail.com>
2023-08-13 15:29:28 +0500

    middleware added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express'); 
 const app = express(); 
-app.get("/",(req,res)=>{
+
+const middleware=(req,res,next)=>{
+    console.log("Middleware is running");
+next();
+}
+
+app.get("/", (req,res)=>{
     res.send("Hello World");
+})
+app.get("/about", middleware,(req,res)=>{
+    console.log("About page is running");
+    res.send("Hello from about page");
 })
 app.listen(5000,()=>{
     console.log("Server is running on port 3000");
